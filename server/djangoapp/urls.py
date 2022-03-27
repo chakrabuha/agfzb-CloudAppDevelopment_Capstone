ddd967458ad536275844006412f39f2cfd9dbdd4
chakrabuha <aritrochakraborty@gmail.com>
2022-03-27 23:17:51 +0000

    kk

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,15 +10,14 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route='about/',view=views.about,name='aboutus'),
     # path for contact us view
-
+    path(route='contact/',view=views.contact,name='contactus'),
     # path for registration
 
     # path for login
 
     # path for logout
-
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
